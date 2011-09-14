0ff13313dd1aab2db6896dc92515263f94393c31
Francesco eFFeeMMe Mastellone <francesco.mastellone@gmail.com>
2011-09-14 10:54:39 +0200

    hark

--- source/platformer.py
+++ source/platformer.py
@@ -1,19 +1,44 @@
-class EventHub:
-    def __init__(self):
-        self._listeners = {}
-    
-    def register_listener(self, listener, *args, **kwargs):
-        if listener in self._listeners:
-            self._listeners[listener].append()
+{"STACK: MAIN MENU": {
+    "EVENT TYPE": {
+        EventInstance: [listeners]
+        }
+    }
+}
 
+
+class EventListener(object):
+    _active_stack = None
+    _event_stacks = {}
+    _event_bindings = {}
+    
+    def add_stack(self, name):
+        _event_stack[name] = {}
+    
+    def remove_stack(self, name):
+        if name in _event_stack:
+            #TODO event stack remove
+            for event_type in _event_stack:
+                pass
+    
+    def event_listen(self, event):
+        
+    
+    def event_emit(self, event):
+        if event.name in _event_bindings
 
 class Event:
-    pass
+    def _respects_pattern_of(self, event):
+        """Override this method to, for instance, check for presses of a key"""
+        return True
 
 class KeyPress(Event):
-    pass
+    name = "KeyPress"
+    def __init__(self, key):
+        
 
-class MousePress
+class MousePress(Event):
+    def __init__(self, button):
+        
 
 
 class Windowing:
